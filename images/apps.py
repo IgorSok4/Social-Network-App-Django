67f40be04201d0cfc751fc3eb17908a7c0b579ed
IgorS <sok.igor4@gmail.com>
2023-02-24 23:53:01 +0100

    implemented signals that helps count amount of likes on each photo

--- images/apps.py
+++ images/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+    def ready(self):
+        # importowanie procedur do obsługi sygnałów
+        import images.signals
