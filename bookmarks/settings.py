c5122ab3a7d26e40675be176e979f92d5d31250d
Igor Sokół <118025302+IgorSok4@users.noreply.github.com>
2023-02-26 20:09:51 +0100

    Update settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd3z5o7$4x9t7&y8xx5jstx5g$ur(mcex8j&=7jn7zxagf=pc06'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
